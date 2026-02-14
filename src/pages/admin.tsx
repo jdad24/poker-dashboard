161a546d024f6d4061e952ab3c8b177fe4156fac
Joshua  Dadson <joshuadadson@Joshuas-MacBook-Air.local>
2026-02-14 12:18:57 -0600

    table resizing

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -3,7 +3,7 @@ import PlayerTransactionsTable from "../components/players/player-transactions-t
 import { useState, useEffect } from "react"
 import { getTransactions } from "../services/transactions"
 import { getPlayers } from "../services/players"
-import {getDealers} from "../services/dealers"
+import { getDealers } from "../services/dealers"
 import { Button } from "@mui/material"
 import NewPlayerModal from "../components/players/new-player-modal"
 import NewTransactionsModal from "../components/new-transactions-modal"
@@ -12,6 +12,7 @@ import DataCard from "../components/data-card"
 import { Tabs, Tab } from "@mui/material"
 import DealersTable from "../components/dealers/dealers-table"
 import NewDealerModal from "../components/dealers/new-dealer-modal"
+import { OpenInFull } from "@mui/icons-material"
 
 export default function AdminPage() {
     const [players, setPlayers] = useState([]);
@@ -20,13 +21,16 @@ export default function AdminPage() {
 
     const [newPlayerModalOpen, setNewPlayerModalOpen] = useState(false);
     const [newDealerModalOpen, setNewDealerModalOpen] = useState(false);
-    const [newTransactionModalOpen, setNewTransactionModalOpen] = useState(false);    
+    const [newTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
 
     const [personnelTab, setPersonnelTab] = useState(0);
 
+    const [personnelTabExpanded, setPersonnelTabExpanded] = useState(true);
+    const [transactionsTabExpanded, setTransactionsTabExpanded] = useState(true);
+
     useEffect(() => {
         const fetchData = async () => {
-            try {                
+            try {
                 setPlayers(await getPlayers());
                 setDealers(await getDealers());
                 setTransactions(await getTransactions());
@@ -63,21 +67,25 @@ export default function AdminPage() {
         }
     }
     return (
-        <div className="flex flex-col w-[95%] mx-auto">
+        <div className="flex flex-col w-[95%] h-auto min-h-screen mx-auto">
             <div className="flex flex-row justify-between mb-12 ">
                 <DataCard className="m-4 shadow-md shadow-black" title="Total Players" value={String(players.length)} />
                 <DataCard className="m-4 shadow-md shadow-black" title="Total Dealers" value={String(dealers.length)} />
                 <DataCard className="m-4 shadow-md shadow-black" title="Total Transactions" value={String(transactions.length)} />
             </div>
             <div className="flex flex-col space-y-10">
-                <div className="bg-blue-400/20 p-4 rounded-lg shadow-md shadow-black">
-                    <Tabs onChange={handlePersonnelTabChange} className="mb-4" value={personnelTab}>
-                        <Tab className="font-bold" label="Players" value={0} />
-                        <Tab className="font-bold" label="Dealers" value={1} />
-                    </Tabs>
+                <div className={`bg-blue-400/20 p-4 rounded-lg shadow-md shadow-black ${personnelTabExpanded ? 'h-auto' : 'h-20'} overflow-hidden`}>
+                    <div className="flex flex-row justify-between">
+                        <Tabs onChange={handlePersonnelTabChange} className="mb-4" value={personnelTab}>
+                            <Tab className="font-bold" label="Players" value={0} />
+                            <Tab className="font-bold" label="Dealers" value={1} />
+                        </Tabs>
+                        <OpenInFull className="ml-auto hover:scale-150 cursor-pointer" onClick={() => setPersonnelTabExpanded(!personnelTabExpanded)} />
+                    </div>
                     {renderPeronnelTabContent()}
                 </div>
-                <div className="bg-blue-400/20 p-4 rounded-lg shadow-md shadow-black">
+                <div className={`flex flex-col bg-blue-400/20 p-4 rounded-lg shadow-md shadow-black ${transactionsTabExpanded ? 'h-auto' : 'h-25'} overflow-hidden`}>
+                    <OpenInFull className="ml-auto hover:scale-150 cursor-pointer" onClick={() => setTransactionsTabExpanded(!transactionsTabExpanded)} />
                     <div className="text-xl font-semibold flex flex-row items-center justify-between">
                         <p className="text-xl">Transactions</p>
                         <Button size="small" variant="contained" className="w-50 my-4 mr-4 ml-auto" onClick={() => setNewTransactionModalOpen(true)}>Add Transaction <Add className="ml-2" /></Button>
