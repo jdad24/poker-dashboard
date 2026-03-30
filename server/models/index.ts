1b3dc5b7026bd391c10a978b32128b9907d057cd
Joshua Dadson <joshuadadson@yahoo.com>
2026-03-30 18:20:20 -0500

    path fix

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,6 +1,6 @@
-import pool from "../db/index";
+import pool from "../db/index.ts";
 import bcrypt from 'bcrypt';
-import type { User as UserType } from "../../types/index";
+import type { User as UserType } from "../../types/index.ts";
 
 export class User {
     id: number;
